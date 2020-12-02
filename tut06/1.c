2496e0d0c205bab05db4e6a70baf2374999afcbe
markuswt <53354649+markuswt@users.noreply.github.com>
2020-12-02 01:28:21 +0100

    Fix format of tut06/1.c

--- tut06/1.c
+++ tut06/1.c
@@ -1,6 +1,6 @@
 void palindrom1(char feld[], int l, int* korrekt) {
     int i = 0;
-    l = l − 1;
+    l = l - 1;
     *korrekt = 1;
     while (i < l / 2 && (*korrekt)) {
         (*korrekt) = feld[i] == feld[l];
